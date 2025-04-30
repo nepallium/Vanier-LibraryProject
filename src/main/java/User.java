3a37ba4967af8c8b473c3bd5ee33e9f2bae323df
nepallium <122187419+nepallium@users.noreply.github.com>
2025-04-30 19:25:36 -0400

    Complete User class definition

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,2 +1,16 @@
+import lombok.*;
+
+@EqualsAndHashCode
+@ToString
+@Getter
+@Setter
 abstract class User {
+    private String name;
+    private Gender gender;
+    private int id;
+
+    public User(String name, Gender gender) {
+        this.name = name;
+        this.gender = gender;
+    }
 }
