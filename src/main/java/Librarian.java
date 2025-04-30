67056e6d7910a61b06dc5166ef9e06d65ec1e01d
nepallium <122187419+nepallium@users.noreply.github.com>
2025-04-30 19:54:50 -0400

    Add extra methods to LibrarySystem

--- src/main/java/Librarian.java
+++ src/main/java/Librarian.java
@@ -1,2 +1,30 @@
-public class Librarian {
+import lombok.*;
+
+@EqualsAndHashCode
+@ToString
+@Getter
+@Setter
+public class Librarian extends User {
+    private static int nextId = 1;
+
+    public Librarian(String name, Gender gender) {
+        super(name, gender);
+        this.setId(nextId++);
+    }
+
+    /**
+     * Adds new books from an input csv file into the library's list of books
+     *
+     * @param path
+     */
+    public void addNewBooksToLibrary(String path) {
+        //TODO
+    }
+
+    /**
+     * Processes a returned book from the library's queue of returned books to make it available again
+     */
+    public void processReturn() {
+        //TODO
+    }
 }
