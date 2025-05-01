570ebca31b00fba5c56f1ef89868ddddeaa6f0c5
nepallium <122187419+nepallium@users.noreply.github.com>
2025-04-30 20:27:30 -0400

    Complete Book class definition

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,2 +1,39 @@
+import lombok.*;
+
+import java.util.Comparator;
+
+@EqualsAndHashCode
+@Getter
+@Setter
 abstract class Book {
+    private String title;
+    private Author author;
+    private String isbn;
+    private int pages;
+    private Issuable.Status status;
+
+    public Book(String title, Author author, String isbn, int pages) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.pages = pages;
+        this.status = Issuable.Status.AVAILABLE;
+    }
+
+    public abstract void displayBook();
+
+    @AllArgsConstructor
+    public static class BookComparator implements Comparator<Book> {
+        private String criteria;
+
+        @Override
+        public int compare(Book o1, Book o2) {
+            return switch (criteria) {
+                case "title" -> o1.title.compareTo(o2.title);
+                case "pages" -> o1.pages - o2.pages;
+                case "author" -> o1.author.compareTo(o2.author);
+                default -> o1.isbn.compareTo(o2.isbn);
+            };
+        }
+    }
 }
