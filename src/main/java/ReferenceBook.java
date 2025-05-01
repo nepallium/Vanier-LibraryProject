4d40452b88b49bd757f9d1de771d7fb3355de4ee
nepallium <122187419+nepallium@users.noreply.github.com>
2025-04-30 20:19:54 -0400

    Complete Reference Book class definition

--- src/main/java/ReferenceBook.java
+++ src/main/java/ReferenceBook.java
@@ -1,2 +1,33 @@
-public class ReferenceBook {
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@EqualsAndHashCode(callSuper = true)
+@Getter
+@Setter
+public class ReferenceBook extends Book {
+    private String shelfLocation;
+    private int totalCopies;
+
+    public ReferenceBook(String isbn, String title, int pages, Author author, int totalCopies, String shelfLocation) {
+        super(isbn, title, pages, author);
+        this.totalCopies = totalCopies;
+        this.shelfLocation = shelfLocation;
+    }
+
+    /**
+     * Displays relevant info about the reference book
+     */
+    @Override
+    public void displayBook() {
+        String info = String.format("%s by %s\n\t" +
+                "Shelf location: %s\n\t" +
+                "Total copies: %d\n\t" +
+                "Pages: %d\n\t" +
+                "NOTE: Book is not issuable, put it back place after use",
+                getTitle(), getAuthor().getName(), shelfLocation, totalCopies, getPages());
+
+        System.out.println(info);
+    }
 }
