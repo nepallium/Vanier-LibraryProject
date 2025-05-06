e1b61f69a064a5ec3be2c72ad8d51ce5b960abbc
nepallium <122187419+nepallium@users.noreply.github.com>
2025-05-05 20:33:18 -0400

    Implement Student methods

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,9 +1,11 @@
 import lombok.*;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-@EqualsAndHashCode(callSuper = true)
+//@EqualsAndHashCode(callSuper = true)
 @ToString
 @Getter
 public class Student extends User {
@@ -23,8 +25,21 @@ public class Student extends User {
      * @return
      */
     public boolean borrowBook(NormalBook book) {
-        //TODO set due date once borrowed
-        return true;
+        if (book == null) {
+            throw new IllegalArgumentException("book must not be null");
+        }
+
+        if (!book.isAvailable() || borrowedBooks.contains(book)) {
+            return false;
+        }
+
+        LocalDate due = LocalDate.now().plusWeeks(book.getLoanPeriodWeeks());
+        book.setDueDate(due);
+
+        book.setCurrentBorrower(this);
+        book.setStatus(Issuable.Status.BORROWED);
+
+        return borrowedBooks.add(book);
     }
 
     /**
@@ -33,8 +48,18 @@ public class Student extends User {
      * @param book the book to return
      */
     public boolean returnBook(NormalBook book) {
-        //TODO mark book as available
-        return true;
+        if (book == null) {
+            throw new IllegalArgumentException("book must not be null");
+        }
+
+        if (borrowedBooks.remove(book)) {
+            book.setStatus(Issuable.Status.PROCESSING);
+            book.setDueDate(null);
+            LibrarySystem.returnedBooks.offer(book);
+            return true;
+        }
+
+        return false;
     }
 
     /**
@@ -43,7 +68,23 @@ public class Student extends User {
      * @return the sorted list of books
      */
     public List<NormalBook> getLoansByDueDate() {
-        //TODO
-        return null;
+        return borrowedBooks.stream()
+                .filter(Objects::nonNull)
+                .sorted()
+                .toList();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Student student = (Student) o;
+        return Objects.equals(borrowedBooks, student.borrowedBooks);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), borrowedBooks);
     }
 }
