5042f89c1693dd3702deedd0ffd01983b4359a74
nepallium <122187419+nepallium@users.noreply.github.com>
2025-05-06 21:14:54 -0400

    Add java docs

--- src/main/java/Author.java
+++ src/main/java/Author.java
@@ -14,6 +14,12 @@ public class Author implements Comparable<Author> {
         this.name = name;
     }
 
+    /**
+     * Sorts authors by name, and, if equal, by age, ascending
+     *
+     * @param o the object to be compared.
+     * @return the comparison result
+     */
     @Override
     public int compareTo(Author o) {
         return (name.compareTo(o.name)) * 1000 +
